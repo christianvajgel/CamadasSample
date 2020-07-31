896f7f93fc40432e6ed383a9c53f038b56fb4b77
Christian Vajgel <christian.vajgel@al.infnet.edu.br>
2020-07-31 14:18:22 -0300

    Add project files.

---
+++ Web/Models/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class Aluno
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Email { get; set; }
+        public Status Status { get; set; }
+
+    }
+
+    public enum Status 
+    {
+        NAO_ATIVO,
+        ATIVO,
+        EM_CONFIRMACAO_EMAIL
+    }
+}
---
+++ Web/Repository/AlunoRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web.Models;
+
+namespace Web.Repository
+{
+    public class AlunoRepository
+    {
+        private static List<Aluno> Alunos { get; set; }
+
+        public List<Aluno> GetAll() 
+        {
+            return Alunos;
+        }
+
+        public Aluno GetAlunoById(Guid id) 
+        {
+            return Alunos.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Save(Aluno aluno) 
+        {
+            Alunos.Add(aluno);
+        }
+
+        public void Delete(Guid id) 
+        {
+            var aluno = this.GetAlunoById(id);
+
+            if (aluno != null) 
+            {
+                Alunos.Remove(aluno);
+            }
+        }
+    }
+}
